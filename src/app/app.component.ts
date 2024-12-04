ab0fee86dbed38fd68f63908243282e364b6a06c
Asad-Ali <selcouth683@gmail.com>
2024-12-04 12:37:59 +0500

    I've added a Gruvbox theme to the application globally. so I don't have to write the styles for each n everycomponent. + creating html form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'list_Sorter';
+  // Variables
+  title = 'Sorting Lists By Merge Sort';
+  inputString: string = '';
+  inputList: number[] = [];
+  sortedList:number[] = [];
+
+  // constructor:
+  constructor() {};
+
+  // Class methods
+  onSubmit() {
+
+
+  }
 }
